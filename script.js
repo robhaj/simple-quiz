bbc896bad30ab9887b21ed0df48170914cd684a7
Robert Hajek <hajek.rob@gmail.com>
2015-07-28 17:01:55 -0600

    'Too much bacon' - no one ever

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 function submitAnswers() {
-  var total = 5;
+  var total = 6;
   var score = 0;
   //input
-  var q1 = document.forms["quizForm"]["q1"].value;
-  var q2 = document.forms["quizForm"]["q2"].value;
+  var formResult = document.forms["quizForm"];
+  var q1 = formResult.["q1"].value;
   var q3 = document.forms["quizForm"]["q3"].value;
   var q4 = document.forms["quizForm"]["q4"].value;
   var q5 = document.forms["quizForm"]["q5"].value;
-
+  var q6 = document.forms["quizForm"]["q6"].value;
   //validation
   for (i=1; i<=total; i++ ) {
-    if (eval('q'+i) == null || (eval('q'+i) == "")) {
+    if (eval('q'+i) === null || (eval('q'+i) == "")) {
       alert("You missed question " + i);
       return false;
     }
   }
 
   //set answer key
-  var answers = ["a","c","c","d","d"];
+  var answers = ["a","c","c","d","d","d"];
 
   //check answers(process)
   for (i=1; i<=total; i++) {
 
-    if (eval('q'+i)==answers[i - 1]) {
+    if (eval('q'+i)===answers[i - 1]) {
       score++
   }
 }
@@ -30,7 +30,7 @@ function submitAnswers() {
 
 // output
 var results = document.getElementById("results");
-results.innerHTML = '<h3>You scored ' +score+ ' out of ' +total + '</h3>'
+results.innerHTML = '<h3>You scored ' +score+ ' out of ' +total+ '</h3>'
 alert("You scored " +score+ " out of " +total)
 return false;
 }
