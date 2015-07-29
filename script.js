ac16f9d604d5ecd07000bab138087dfac5deab4a
Robert Hajek <hajek.rob@gmail.com>
2015-07-29 15:38:29 -0600

    refactored DOM output in jquery

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+$(document).ready(function() {
+
+  //audio
+  $('.glyphicon-volume-off').on('click', function(){
+    $('.audio').prop("volume", 0);
+  });
+  $('.glyphicon-volume-up').on('click', function(){
+    $('.audio').prop("volume", 1);
+  });
+
+  //output
+
+});
+  //submit function
 function submitAnswers() {
   //set answer key
   var answers = ["a","c","c","d","d","d"];
-
   var total = answers.length;
   var score = 0;
+
   //input
   var formResult = document.forms["quizForm"];
   var q1 = formResult["q1"].value;
@@ -12,6 +26,7 @@ function submitAnswers() {
   var q4 = formResult["q4"].value;
   var q5 = formResult["q5"].value;
   var q6 = formResult["q6"].value;
+
   //validation
   for (i=1; i<=total; i++ ) {
     if (eval('q'+i) === null || (eval('q'+i) === "")) {
@@ -20,7 +35,7 @@ function submitAnswers() {
     }
   }
 
-  //check answers(process)
+  //check answers and increment total
   for (i=1; i<=total; i++) {
 
     if (eval('q'+i)===answers[i - 1]) {
@@ -28,27 +43,18 @@ function submitAnswers() {
     }
   }
 
-  // output
-  var results = document.getElementById("results");
-  results.innerHTML = '<h3>You scored ' +score+ ' out of ' +total+ '</h3>';
+  //output
+  $('section #results').html('<h3>You scored ' +score+ ' out of ' +total+ '</h3>');
   alert("You scored " +score+ " out of " +total);
-  return false;
+return false;
 }
 
-$(document).ready(function() {
-  $('.glyphicon-volume-off').on('click', function(){
-    $('.audio').prop("volume", 0);
-  });
-  $('.glyphicon-volume-up').on('click', function(){
-    $('.audio').prop("volume", 1);
-  });
-});
 //   function progressBar() {
-//     for (var i=0; i>=100; i+=20) {
-//
+//   var width =  $('.progress-bar').css("width");
+//     console.log(width);
+// }
 //     $('.progress-bar').css("width", (i+"%")).attr('aria-valuenow', i);
 // }
-//   }
+//
 //   progressBar();
 //   // if ($('#q1a').attr('checked') === true)
-// });
