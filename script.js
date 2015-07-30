22e39afe8a3105430f72f9f5251378ae255faf03
Robert Hajek <hajek.rob@gmail.com>
2015-07-30 15:09:05 -0600

    dynamic progress bar working

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 $(document).ready(function() {
 
-  //audio
-  $('.glyphicon-volume-off').on('click', function(){
-    $('.audio').prop("volume", 0);
-  });
-  $('.glyphicon-volume-up').on('click', function(){
-    $('.audio').prop("volume", 1);
-  });
+//set total # of questions
+  var total = 6;
 
-  //output
-
+//dynamic progress bar
+$('input').on('change', function(){
+  var increment = 100/total;
+  var bar = $('input:checked').length;
+  $('.progress-bar').css("width", bar*increment+"%");
 });
+
+  //audio off
+  $('.glyphicon-volume-off').on('click', function(){
+    $('.audio').prop("volume", 0);});
+
+  //audio on
+  $('.glyphicon-volume-up').on('click', function(){
+    $('.audio').prop("volume", 1);});
+
+  //end document ready function
+});
+
   //submit function
 function submitAnswers() {
-  //set answer key
   var answers = ["a","c","c","d","d","d"];
   var total = answers.length;
   var score = 0;
 
-  //input
+  //grab input
   var formResult = document.forms["quizForm"];
   var q1 = formResult["q1"].value;
   var q2 = formResult["q2"].value;
@@ -31,30 +40,17 @@ function submitAnswers() {
   for (i=1; i<=total; i++ ) {
     if (eval('q'+i) === null || (eval('q'+i) === "")) {
       alert("You missed question " + i);
-      return false;
-    }
+      return false;}
   }
 
   //check answers and increment total
   for (i=1; i<=total; i++) {
-
     if (eval('q'+i)===answers[i - 1]) {
-      score++;
+      score++;}
     }
-  }
 
   //output
   $('section #results').html('<h3>You scored ' +score+ ' out of ' +total+ '</h3>');
   alert("You scored " +score+ " out of " +total);
 return false;
 }
-
-//   function progressBar() {
-//   var width =  $('.progress-bar').css("width");
-//     console.log(width);
-// }
-//     $('.progress-bar').css("width", (i+"%")).attr('aria-valuenow', i);
-// }
-//
-//   progressBar();
-//   // if ($('#q1a').attr('checked') === true)
