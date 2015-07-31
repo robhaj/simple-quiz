c619a46e0eca04e26e6b36be8900f9800f00f8fe
Robert Hajek <hajek.rob@gmail.com>
2015-07-31 00:21:42 -0600

    finished create questions/answer functions

---
+++ objects.js
@@ -0,0 +1,18 @@
+console.log('sanity check');
+var questions = [
+  {q:"1. Which of the following is not a primitive?", a:"a", options:4},
+  {q:"2. Which of the following is not a monkey?", a:"a"},
+  {q:"3. Which of the following is not a banana?", a:"a"}
+];
+
+var answers = [
+  {ans:"Arrays", name:"q1", value:"a", id:"q1a"},
+  {ans:"Integers", name:"q1", value:"a", id:"q1b"},
+  {ans:"Arrays", name:"q1", value:"a", id:"q1c"},
+  {ans:"Arrays", name:"q1", value:"a", id:"q1d"},
+  {ans:"Arrays", name:"q2", value:"a", id:"q2a"},
+  {ans:"Arrays", name:"q2", value:"a", id:"q2b"},
+  {ans:"Arrays", name:"q2", value:"a", id:"q2c"},
+  {ans:"Arrays", name:"q2", value:"a", id:"q2d"},
+  {ans:"Arrays", name:"q3", value:"a", id:"q3a"},
+];
--- script.js
+++ script.js
@@ -1,56 +1,58 @@
-$(document).ready(function() {
+  //progress bar dynamics
+  function progBar(){
+    $('input').on('change', function(){
+      var increment = 50;
+      var bar = $('input:checked').length;
+      $('.progress-bar').css("width", bar*increment+"%");
+    });
+  }
 
-//set total # of questions
-  var total = 6;
-
-//dynamic progress bar
-$('input').on('change', function(){
-  var increment = 100/total;
-  var bar = $('input:checked').length;
-  $('.progress-bar').css("width", bar*increment+"%");
-});
-
-  //audio off
+  //audio off button event
   $('.glyphicon-volume-off').on('click', function(){
     $('.audio').prop("volume", 0);});
 
-  //audio on
-  $('.glyphicon-volume-up').on('click', function(){
-    $('.audio').prop("volume", 1);});
+    //audio on button event
+    $('.glyphicon-volume-up').on('click', function(){
+      $('.audio').prop("volume", 1);
+    });
 
-  //end document ready function
-});
-
-  //submit function
-function submitAnswers() {
-  var answers = ["a","c","c","d","d","d"];
-  var total = answers.length;
-  var score = 0;
-
-  //grab input
-  var formResult = document.forms["quizForm"];
-  var q1 = formResult["q1"].value;
-  var q2 = formResult["q2"].value;
-  var q3 = formResult["q3"].value;
-  var q4 = formResult["q4"].value;
-  var q5 = formResult["q5"].value;
-  var q6 = formResult["q6"].value;
-
-  //validation
-  for (i=1; i<=total; i++ ) {
-    if (eval('q'+i) === null || (eval('q'+i) === "")) {
-      alert("You missed question " + i);
-      return false;}
-  }
-
-  //check answers and increment total
-  for (i=1; i<=total; i++) {
-    if (eval('q'+i)===answers[i - 1]) {
-      score++;}
+    //create question
+    function createQuestion(text){
+      $('.qst1').append("<h3>" +text+ "</h3>");
     }
 
-  //output
-  $('section #results').html('<h3>You scored ' +score+ ' out of ' +total+ '</h3>');
-  alert("You scored " +score+ " out of " +total);
-return false;
+    //create answer
+    function createAnswers(num, letter, id, answer){
+      $('.qst1').append('<input type="radio" name='+num+' value='+letter+' id='+id+' />'+answer+'<br>');
+    }
+
+  function submitAnswers() {
+
+    //cache
+    var score = 0;
+    var formResult = document.forms["quizForm"];
+    var q1 = formResult["q1"].value;
+    var q2 = formResult["q2"].value;
+    var q3 = formResult["q3"].value;
+    var q4 = formResult["q4"].value;
+    var q5 = formResult["q5"].value;
+    var q6 = formResult["q6"].value;
+
+    //check answers and calculate score
+    for (i=1; i<=total; i++) {
+      if (eval('q'+i)===answers[i - 1]) {
+        score++;}
+      }
+
+      //validation
+      for (i=1; i<=total; i++ ) {
+        if (eval('q'+i) === null || (eval('q'+i) === "")) {
+          alert("You missed question " + i);
+          return 1;}
+        }
+
+        //output
+        $('section #results').html('<h3>You scored ' +score+ ' out of ' +total+ '</h3>');
+        alert("You scored " +score+ " out of " +total);
+        return 1;
 }
