ff4cca378d2a719c27827ea0fe59ffe5d0c3bd2e
Robert Hajek <hajek.rob@gmail.com>
2015-07-31 14:02:30 -0600

    finished refactoring to jquery

--- objects.js
+++ objects.js
@@ -1,18 +1,13 @@
 console.log('sanity check');
 var questions = [
-  {q:"1. Which of the following is not a primitive?", a:"a", options:4},
-  {q:"2. Which of the following is not a monkey?", a:"a"},
-  {q:"3. Which of the following is not a banana?", a:"a"}
-];
-
-var answers = [
-  {ans:"Arrays", name:"q1", value:"a", id:"q1a"},
-  {ans:"Integers", name:"q1", value:"a", id:"q1b"},
-  {ans:"Arrays", name:"q1", value:"a", id:"q1c"},
-  {ans:"Arrays", name:"q1", value:"a", id:"q1d"},
-  {ans:"Arrays", name:"q2", value:"a", id:"q2a"},
-  {ans:"Arrays", name:"q2", value:"a", id:"q2b"},
-  {ans:"Arrays", name:"q2", value:"a", id:"q2c"},
-  {ans:"Arrays", name:"q2", value:"a", id:"q2d"},
-  {ans:"Arrays", name:"q3", value:"a", id:"q3a"},
+  {q:"1. Which of the following is not a primitive?", answers:["Arrays", "Integers", "Strings", "Functions"], correct:"Arrays"},
+  {q:"2. Which of the following is not a primitive?", answers:["Arrays", "Integers", "Strings", "Functions"], correct:"Arrays"},
+  {q:"3. Which of the following is not a primitive?", answers:["Arrays", "Integers", "Strings", "Functions"], correct:"Arrays"},
+  {q:"4. Which of the following is not a primitive?", answers:["Arrays", "Integers", "Strings", "Functions"], correct:"Arrays"},
+  {q:"5. Which of the following is not a primitive?", answers:["Arrays", "Integers", "Strings", "Functions"], correct:"Arrays"},
+  {q:"6. Which of the following is not a primitive?", answers:["Arrays", "Integers", "Strings", "Functions"], correct:"Arrays"},
+  {q:"7. Which of the following is not a primitive?", answers:["Arrays", "Integers", "Strings", "Functions"], correct:"Arrays"},
+  {q:"8. Which of the following is not a primitive?", answers:["Arrays", "Integers", "Strings", "Functions"], correct:"Arrays"},
+  {q:"9. Which of the following is not a primitive?", answers:["Arrays", "Integers", "Strings", "Functions"], correct:"Arrays"},
+  {q:"10. Which of the following is not a primitive?", answers:["Arrays", "Integers", "Strings", "Functions"], correct:"Arrays"},
 ];
--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-  //progress bar dynamics
-  function progBar(){
-    $('input').on('change', function(){
-      var increment = 50;
-      var bar = $('input:checked').length;
-      $('.progress-bar').css("width", bar*increment+"%");
-    });
-  }
+appendQuestions();
+progBar();
+audioEvents();
 
+$('form').on('submit', function(e) {
+  e.preventDefault();
+  submitAnswers();
+});
+
+//create question
+function createQuestion(text){
+  $('.qst').append("<h3>" +text+ "</h3>");
+}
+
+//create answer
+function createAnswers(id, answer, name){
+  $('.qst').append('<input type="radio" name='+name+ ' value='+answer+ ' name="" id='+id+' /> <span>' +answer+'</span><br>');
+}
+
+//display all questions/answers in objects.js
+function appendQuestions() {
+  var k = 0;
+  for (var l = 0; l < questions.length; l++) {
+    createQuestion(questions[l].q);
+    for (var i = 0; i < questions[i].answers.length; i++ ) {
+      createAnswers(i+"a"+l,questions[i].answers[k], l);
+      k++;
+    }
+    k=0;
+  }
+}
+
+// progress bar dynamics
+function progBar(){
+  $('input').on('change', function(){
+    var increment = 100 /questions.length;
+    var bar = $('input:checked').length;
+    $('.progress-bar').css("width", bar*increment+"%");
+  });
+}
+
+function audioEvents() {
   //audio off button event
   $('.glyphicon-volume-off').on('click', function(){
     $('.audio').prop("volume", 0);});
@@ -14,45 +47,34 @@
     //audio on button event
     $('.glyphicon-volume-up').on('click', function(){
       $('.audio').prop("volume", 1);
-    });
-
-    //create question
-    function createQuestion(text){
-      $('.qst1').append("<h3>" +text+ "</h3>");
     }
-
-    //create answer
-    function createAnswers(num, letter, id, answer){
-      $('.qst1').append('<input type="radio" name='+num+' value='+letter+' id='+id+' />'+answer+'<br>');
-    }
-
-  function submitAnswers() {
-
-    //cache
-    var score = 0;
-    var formResult = document.forms["quizForm"];
-    var q1 = formResult["q1"].value;
-    var q2 = formResult["q2"].value;
-    var q3 = formResult["q3"].value;
-    var q4 = formResult["q4"].value;
-    var q5 = formResult["q5"].value;
-    var q6 = formResult["q6"].value;
-
-    //check answers and calculate score
-    for (i=1; i<=total; i++) {
-      if (eval('q'+i)===answers[i - 1]) {
-        score++;}
-      }
-
-      //validation
-      for (i=1; i<=total; i++ ) {
-        if (eval('q'+i) === null || (eval('q'+i) === "")) {
-          alert("You missed question " + i);
-          return 1;}
-        }
-
-        //output
-        $('section #results').html('<h3>You scored ' +score+ ' out of ' +total+ '</h3>');
-        alert("You scored " +score+ " out of " +total);
-        return 1;
+  );
 }
+
+//tracks score
+function submitAnswers() {
+
+  //cache
+  var total = questions.length;
+  var score = 0;
+  var checked = $('input:checked');
+  var akey = [questions[0].correct, questions[1].correct, questions[2].correct, questions[3].correct, questions[4].correct, questions[5].correct, questions[6].correct, questions[7].correct, questions[8].correct, questions[9].correct];
+
+  //check answers and calculate score
+  for (var i=0; i<total; i++) {
+    if (akey[i]===checked[i].value) {
+      score++;
+    }
+  }
+
+  //output
+  $('#results').html('<h3>You scored ' +score+ ' out of ' +total+ '</h3>');
+  alert("You scored " +score+ " out of " +total);
+}
+
+//validation
+// for (j=1; j<=total; j++ ) {
+//   if (checked[j-1]) {
+//     alert("You missed question " + j);
+//     return 1;
+// }}
